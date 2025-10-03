0add853b0f1cb191480492b8159d8c5612b2221f
Aleksander Chaika <alekschayka97@gmail.com>
2025-10-03 14:25:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function centerSpider() {
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  const left = (wallWidth - spiderWidth) / 2;
+  const topPos = (wallHeight - spiderHeight) / 2;
+
+  spider.style.position = 'absolute';
+  spider.style.left = `${left}px`;
+  spider.style.top = `${topPos}px`;
+}
+
+if (wall && spider) {
+  centerSpider();
+  window.addEventListener('resize', centerSpider);
+}
